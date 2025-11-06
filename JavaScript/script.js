6bf4ee4574fd81ea41e94c2c7f67071ef1b48eae
Vaishali_Vala <vaishalivala1703@gmail.com>
2025-11-06 17:34:38 +0530

    initial commit

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,27 +1,3 @@
-const recipe = () => {
-  fetch('https://dummyjson.com/recipes')
-    .then(response => response.json())
-    .then(data => {
-      const recipesList = Object.values(data.recipes);
-      const contain = document.getElementById("recipe-container");
-
-      recipesList.forEach(item => {
-        const recipeDiv = document.createElement("div");
-        recipeDiv.innerHTML = `
-          <img src="${item.image}" alt="${item.name}">
-          <h2>${item.name}</h2>
-        `;
-        contain.appendChild(recipeDiv);
-      });
-    })
-    .catch(error => {
-      console.log("Error fetching API:", error);
-    });
-};
-
-document.addEventListener("DOMContentLoaded", recipe);
-
-
 // ----- HEADER -----
 const header = document.createElement("header");
 header.innerHTML = `
@@ -29,7 +5,7 @@ header.innerHTML = `
   <nav>
     <ul>
       <li><a href="#">Home</a></li>
-      <li><a href="#">Recipes</a></li>
+      <li><a href="./Pages/all-recipes.html">Recipes</a></li>
       <li><a href="#">About</a></li>
       <li><a href="#">Contact</a></li>
     </ul>
@@ -37,8 +13,38 @@ header.innerHTML = `
 `;
 document.body.prepend(header); // adds header at top of body
 
+const recipe = () => {
+  fetch('https://dummyjson.com/recipes')
+    .then(response => response.json())
+    .then(data => {
+      const recipesList = Object.values(data.recipes);
+      const container = document.getElementById("recipe-container");
+
+      recipesList.forEach(item => {
+        const recipeDiv = document.createElement("div");
+        //  Bootstrap classes for 3 column
+        recipeDiv.classList.add("col-md-4", "col-sm-6", "col-12");
+        recipeDiv.innerHTML = `
+          <div class="card h-100 shadow-sm position-relative overflow-hidden">
+            <img src="${item.image}" class="card-img-top" alt="${item.name}">
+            <div class="overlay d-flex justify-content-center align-items-center">
+              <span class="material-symbols-outlined fs-1 search-icon p-2" style="background-color: rgba(31, 32, 31, 0.4)"><a href="./Pages/all-recipes.html">search</a></span>
+            </div>
+            <div class="card-body text-center mt-4">
+              <h4 class="card-title fw-bold mt-3">${item.name}</h4>
+            </div>
+          </div>
+        `;
+        container.appendChild(recipeDiv);
+      });
+    })
+    .catch(error => console.log("Error fetching API:", error));
+};
+
 // ----- FOOTER -----
 const footer = document.createElement("footer");
 const year = new Date().getFullYear();
 footer.innerHTML = `&copy; ${year} Vaishali’s Recipe Page.`;
 document.body.appendChild(footer);
+
+document.addEventListener("DOMContentLoaded", recipe);
